1adf054773c6997ad0163e9a191299ca7054864e
ShivamKumarJha <jha.shivam3@gmail.com>
2019-11-11 14:46:31 +0000

    sp9850ka_1h10_oversea-user 8.1.0 OPM2.171019.012 24521 release-keys
    Signed-off-by: ShivamKumarJha <jha.shivam3@gmail.com>

---
+++ BoardConfig.mk
@@ -0,0 +1,8 @@
+DEVICE_PATH := device/digma/LS5040PL
+BOARD_VENDOR := digma
+
+# HIDL
+DEVICE_MANIFEST_FILE := $(DEVICE_PATH)/manifest.xml
+DEVICE_MATRIX_FILE := $(DEVICE_PATH)/compatibility_matrix.xml
+
+-include vendor/digma/LS5040PL/BoardConfigVendor.mk
